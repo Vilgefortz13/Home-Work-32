5aa16d74b329d8e782ba64e2e2b20e70671b148c
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-17 11:41:54 +0600

    filling vending machine with drinks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    static List<VendingMachine> drinks = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run();
+    }
+
+    private static void run() {
+        for (int i = 0; i < 20; i++) {
+            int random = new Random().nextInt(4) + 1;
+            switch (random){
+                case 1 -> drinks.add(new Water("Water", 35));
+                case 2 -> drinks.add(new Coffee("Coffee", 70));
+                case 3 -> drinks.add(new Milk("Milk", 50));
+                case 4 -> drinks.add(new Tea("Tea", 55));
+                default -> System.out.println();
+            }
+        }
+
+        drinks.forEach(System.out::println);
     }
 }
