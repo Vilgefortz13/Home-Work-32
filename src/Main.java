f0345f3a0295d8698d7048f3ba40553c8b1715d2
Rinat Uzanzaev <wilax175@gmail.com>
2022-07-17 11:48:37 +0600

    drinks are moved to a separate array and a method for printing drinks is added

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,27 @@ public class Main {
     }
 
     private static void run() {
+        VendingMachine[] vMachine = {
+                new Water("Water", 35),
+                new Coffee("Coffee", 70),
+                new Milk("Milk", 50),
+                new Tea("Tea", 55)
+        };
         for (int i = 0; i < 20; i++) {
             int random = new Random().nextInt(4) + 1;
             switch (random){
-                case 1 -> drinks.add(new Water("Water", 35));
-                case 2 -> drinks.add(new Coffee("Coffee", 70));
-                case 3 -> drinks.add(new Milk("Milk", 50));
-                case 4 -> drinks.add(new Tea("Tea", 55));
+                case 1 -> drinks.add(vMachine[0]);
+                case 2 -> drinks.add(vMachine[1]);
+                case 3 -> drinks.add(vMachine[2]);
+                case 4 -> drinks.add(vMachine[3]);
                 default -> System.out.println();
             }
         }
+    }
 
-        drinks.forEach(System.out::println);
+    public static void printDrinks(VendingMachine[] list) {
+        for (VendingMachine v : list) {
+            System.out.printf("[%d] - %s%n", v.getPrice(), v.getName());
+        }
     }
 }
